16beb80164c132f8956ffdd9172332eead8eed74
Wayne Keepo <dimapimushkin@mail.ru>
2021-06-06 03:46:14 +0400

    clear build file

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,4 @@
 plugins {
-    id("org.springframework.boot") version "2.5.0"
-    id("io.spring.dependency-management") version "1.0.11.RELEASE"
     java
 }
 
@@ -20,19 +18,13 @@ repositories {
 
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter-web")
-
-    developmentOnly("org.springframework.boot:spring-boot-devtools")
 
     runtimeOnly("org.postgresql:postgresql")
 
     compileOnly("org.projectlombok:lombok")
 
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
-
     annotationProcessor("org.projectlombok:lombok")
-//    testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
-//    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
+
 }
 
 tasks.getByName<Test>("test") {
